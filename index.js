975ed3602608c89a3b7012131f087e2dce435355
CodeScot <github@codescot.com>
2021-02-10 13:20:39 +0000

    Refactored code to be more concise;

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const httpAdapter = require('axios/lib/adapters/http');
 const settle = require('axios/lib/core/settle');
 
 const createCache = ({ db, collectionName, expireAfterSeconds }) => {
+  const axiosCache = db.collection(collectionName);
+  axiosCache.createIndex({ "cachedAt": 1 }, { expireAfterSeconds: expireAfterSeconds });
 
-  const checkCache = async (config) => {
-    const axiosCache = db.collection(collectionName);
-    axiosCache.createIndex({ "cachedAt": 1 }, { expireAfterSeconds: expireAfterSeconds });
-
+  const getFromCache = async (config) => {
     const cachedRequest = await axiosCache.findOne({
       method: config.method,
       url: config.url,
@@ -17,7 +16,7 @@ const createCache = ({ db, collectionName, expireAfterSeconds }) => {
     return cachedRequest;
   }
 
-  const cacheResponse = async (config, response) => {
+  const addCachedResponse = async (config, response) => {
     const axiosCache = db.collection(collectionName);
 
     await axiosCache.insertOne({
@@ -38,29 +37,33 @@ const createCache = ({ db, collectionName, expireAfterSeconds }) => {
     });
   }
 
-  return (config) => {
-    return new Promise(async (resolve, reject) => {
-      const isGetRequest = config.method.toLowerCase() === 'get';
+  async function httpCallWithoutCache(config, cacheCallback) {
+    const response = await httpAdapter(config);
+    cacheCallback && await cacheCallback(config, response);
 
-      if (isGetRequest) {
-        const axiosCache = db.collection(collectionName);
-        axiosCache.createIndex({ "cachedAt": 1 }, { expireAfterSeconds: expireAfterSeconds });
-    
-        const cachedRequest = await checkCache(config);
-        if (cachedRequest) {
-          await settle(resolve, reject, cachedRequest.response);
-          return;
-        }
-      }
-  
-      const response = await httpAdapter(config);
-      if (isGetRequest) {
-        await cacheResponse(config, response);
-      }
-  
-      await settle(resolve, reject, response);
-    })
+    return response;
   }
+
+  return (config) => new Promise(async (resolve, reject) => {
+    let response = null;
+
+    const method = config.method.toLowerCase();
+    switch (method) {
+    case 'get':
+      const cached = await getFromCache(config);
+      if (cached) {
+        response = cached.response;
+      } else {
+        response = await httpCallWithoutCache(config, addCachedResponse)
+      }
+      break;
+    default:
+      response = await httpCallWithoutCache(config, null);
+      break;
+    }
+
+    await settle(resolve, reject, response);
+  });
 }
 
 module.exports = createCache;
